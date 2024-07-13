e2a8e6c00b57e9eb2b160788e5f3171c89401a92
Adam Brennan <58646453+adamjbrennan@users.noreply.github.com>
2024-07-13 15:07:22 -0400

    Add Cracking GeoGuessr Chrome extension

---
+++ extension/popup.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const captureButton = document.getElementById("capture");
+    const screenshotContainer = document.getElementById("screenshotContainer");
+  
+    captureButton.addEventListener("click", function() {
+      chrome.tabs.captureVisibleTab(function(screenshotDataUrl) {
+        const screenshotImage = new Image();
+        screenshotImage.src = screenshotDataUrl;
+        screenshotContainer.appendChild(screenshotImage);
+      });
+    });
+  });
