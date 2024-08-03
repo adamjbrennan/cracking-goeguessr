c0daf76fc251a0e16bc23eede5bbe9e8dd93dd4c
Adam Brennan <58646453+adamjbrennan@users.noreply.github.com>
2024-08-03 14:27:38 -0400

    Add Flask API to receive API call and digest image for processing

---
+++ api/app.py
@@ -0,0 +1,31 @@
+import base64
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+
+@app.route('/cracking-geoguessr/get-recommendation', methods=['POST'])
+@cross_origin()
+def get_recommendation():
+    # Get image.
+    data = request.get_json()
+
+    if 'image' not in data:
+        return jsonify({'error': 'Missing "image" in request body.'}), 400
+
+    image = data['image']
+    
+    # Write image to file system for debugging purposes.
+    file = open('mostRecepnt.png', 'wb') 
+    file.write(base64.b64decode((image))) 
+    file.close() 
+
+    # Use model to get suggestion here.
+
+    # Return suggestion.
+    return 'Seattle, WA'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    cors = CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
