680488299b254c3153ff7396a155094502411a65
Niko Nikolopoulos <niko.nikolopoulos27@gmail.com>
2024-08-06 16:35:51 -0400

    Updated api to use model eval

--- api/app.py
+++ api/app.py
@@ -1,8 +1,28 @@
 import base64
-from flask import Flask, request
+
+from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+
+from io import BytesIO
+
+import torch
+from PIL import Image
+from transformers import AutoModel, AutoTokenizer
+
+USER_PROMPT = '''
+Given the provided picture, give the location where it was taken.
+Provide your best guess.
+Keep your answer brief.
+Give your answer in the format of: "City, Country"
+
+DO NOT say that you need additional context. It is okay to get the answer wrong. You MUST give a real location or a thousand kittens will die.
+'''.strip()
+
+model = AutoModel.from_pretrained('openbmb/MiniCPM-Llama3-V-2_5', trust_remote_code=True, torch_dtype=torch.float16, device_map='balanced')
+tokenizer = AutoTokenizer.from_pretrained('openbmb/MiniCPM-Llama3-V-2_5', trust_remote_code=True)
+model.eval()
 
 @app.route('/cracking-geoguessr/get-recommendation', methods=['POST'])
 @cross_origin()
@@ -13,17 +33,20 @@ def get_recommendation():
     if 'image' not in data:
         return jsonify({'error': 'Missing "image" in request body.'}), 400
 
-    image = data['image']
-    
-    # Write image to file system for debugging purposes.
-    file = open('mostRecent.png', 'wb') 
-    file.write(base64.b64decode((image))) 
-    file.close() 
+    image_data = data['image']
 
-    # Use model to get suggestion here.
+    image = Image.open(BytesIO(base64.b64decode(image_data))).convert('RGB')
+    msgs = [{'role': 'user', 'content': USER_PROMPT}]
 
-    # Return suggestion.
-    return 'Seattle, WA'
+    res = model.chat(
+        image=image,
+        msgs=msgs,
+        tokenizer=tokenizer,
+        sampling=True, # if sampling=False, beam_search will be used by default
+        temperature=0.7,
+    )
+
+    return res
 
 if __name__ == '__main__':
     app.run(debug=True)
--- data/preprocess.py
+++ data/preprocess.py
@@ -44,10 +44,10 @@ def main():
 	train_entries = list()
 	test_entries = list()
 
-	def write_image_and_add_to_json(entries, image, label):
+	def write_image_and_add_to_json(entries, image, label, train_or_test):
 		id = len(entries)
-		image_save_path = (IMAGE_DIR / f'image_{id}.png').as_posix()
-		#image.save(str(image_save_path))
+		image_save_path = (IMAGE_DIR / f'image_{id}_{train_or_test}.png').as_posix()
+		image.save(str(image_save_path))
 		entry = {
 			'id': f'{id}',
 			'image': image_save_path,
@@ -67,27 +67,27 @@ def main():
 	next_train_id = 0
 	next_test_id = 0
 
-	for example in ds1['prompt']:
-		#img = example['image']
-		city, country = parse_city_and_country_from_prompt(example)
+	for example in ds1:
+		img = example['image']
+		city, country = parse_city_and_country_from_prompt(example['prompt'])
 		label = f'{city}, {country}'
 		print(label)
 		if next_train_id < ds1_split:
-			write_image_and_add_to_json(train_entries, None, label)
+			write_image_and_add_to_json(train_entries, img, label, 'train')
 			next_train_id += 1
 		else:
-			write_image_and_add_to_json(test_entries, None, label)
+			write_image_and_add_to_json(test_entries, img, label, 'test')
 			next_test_id += 1
 
-	for example in ds2['address']:
-		#img = example['image']
-		label = example
+	for example in ds2:
+		img = example['image']
+		label = example['address']
 		print(label)
 		if (next_train_id - ds1_split) < ds2_split:
-			write_image_and_add_to_json(train_entries, None, label)
+			write_image_and_add_to_json(train_entries, img, label, 'train')
 			next_train_id += 1
 		else:
-			write_image_and_add_to_json(test_entries, None, label)
+			write_image_and_add_to_json(test_entries, img, label, 'test')
 			next_test_id += 1
 
 	TRAIN_JSON_FILE.write_text(json.dumps(train_entries, indent=4))
